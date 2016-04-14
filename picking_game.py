c8979385e40da00a9ee118780c1c9a4b6f92116b
Matti <mattiadanza@hotmail.com>
2016-04-14 11:28:36 +0100

    Update picking_game.py

--- picking_game.py
+++ picking_game.py
@@ -200,7 +200,7 @@ def run_game():
 
     micrographs = glob.glob('micrographs/*.mrc')
     micrographsdic = {}
-    micrographsbackdic = {}
+    micrographsbackdic = {1:"micrographs/micrograph00007.mrc",2:"micrographs/micrograph00002.mrc"}
     n=3
     for i in micrographs:
         if i not in("micrographs/micrograph00007.mrc","micrographs/micrograph00002.mrc"):
